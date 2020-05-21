34673f86e35d4e95e6dcec684f803b7787948612
libin37 <libin37@58.com>
2020-05-21 18:14:45 +0800

    修改基础类型认证，此处过于简略，后续补充

--- bean-mapper/src/main/java/com/jd/bingo/bean/mapper/builders/map/impl/ParseUtil.java
+++ bean-mapper/src/main/java/com/jd/bingo/bean/mapper/builders/map/impl/ParseUtil.java
@@ -7,11 +7,7 @@ import com.jd.bingo.bean.mapper.utils.BeanUtil;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @description:
@@ -21,6 +17,7 @@ import java.util.Map;
  */
 public class ParseUtil {
     private static List<Class> baseClasses;
+
     static {
         baseClasses = new ArrayList<>();
         baseClasses.add(String.class);
@@ -40,8 +37,9 @@ public class ParseUtil {
         baseClasses.add(Short.class);
         baseClasses.add(Date.class);
     }
-    public static boolean isBaseClass(Class clazs){
-        return clazs.isEnum() || baseClasses.contains(clazs);
+
+    public static boolean isBaseClass(Class clazs) {
+        return clazs.isEnum() || Number.class.isAssignableFrom(clazs) || baseClasses.contains(clazs);
     }
 
     /**
@@ -51,35 +49,35 @@ public class ParseUtil {
      * @param target
      * @return
      */
-    public static List<MapUnit> mapProperties(Class source, Class target){
+    public static List<MapUnit> mapProperties(Class source, Class target) {
         List<Field> fields = BeanUtil.getFields(source);
-        Map<String,MapUnit> map = new HashMap<>();
-        if(fields != null && !fields.isEmpty()){
-            for (Field f: fields){
+        Map<String, MapUnit> map = new HashMap<>();
+        if (fields != null && !fields.isEmpty()) {
+            for (Field f : fields) {
                 MapUnit mu = new MapUnit();
                 String name = getMapFieldName(f);
                 mu.setName(name);
                 mu.setSouce(f.getGenericType());
-                Method method = BeanUtil.getGetMethod(f,source);
-                if(method == null){
+                Method method = BeanUtil.getGetMethod(f, source);
+                if (method == null) {
                     continue;
                 }
                 mu.setGetMethodName(method.getName());
-                map.put(name,mu);
+                map.put(name, mu);
             }
         }
 
         //解析method
-        List<Method> methods = BeanUtil.getMethodsByAnnotation(source,Expressions.class);
-        if(methods != null && !methods.isEmpty()){
-            for (Method m:methods){
+        List<Method> methods = BeanUtil.getMethodsByAnnotation(source, Expressions.class);
+        if (methods != null && !methods.isEmpty()) {
+            for (Method m : methods) {
                 Expressions expressions = m.getAnnotation(Expressions.class);
                 String name = expressions.value();
                 MapUnit mu = map.get(name);
-                if(mu == null){
+                if (mu == null) {
                     mu = new MapUnit();
                     mu.setName(name);
-                    map.put(name,mu);
+                    map.put(name, mu);
                 }
                 mu.setGetMethodName(m.getName());
                 mu.setSouce(m.getGenericReturnType());
@@ -88,37 +86,37 @@ public class ParseUtil {
         //对应target
         List<MapUnit> mus = new ArrayList<>();
         fields = BeanUtil.getFields(target);
-        if(fields != null && !fields.isEmpty()){
-            for (Field f: fields){
+        if (fields != null && !fields.isEmpty()) {
+            for (Field f : fields) {
                 String name = getMapFieldName(f);
                 MapUnit mu = map.get(name);
-                if(mu == null){
+                if (mu == null) {
                     continue;
                 }
                 mu.setTarget(f.getGenericType());
-                Method method = BeanUtil.getSetMethod(f,target);
-                if(method != null){
+                Method method = BeanUtil.getSetMethod(f, target);
+                if (method != null) {
                     mu.setSetMethodName(method.getName());
                     mus.add(mu);
                 }
             }
         }
         //对应method
-        methods = BeanUtil.getMethodsByAnnotation(target,Expressions.class);
-        if(methods != null && !methods.isEmpty()){
-            for (Method m:methods){
+        methods = BeanUtil.getMethodsByAnnotation(target, Expressions.class);
+        if (methods != null && !methods.isEmpty()) {
+            for (Method m : methods) {
                 String name = m.getAnnotation(Expressions.class).value();
                 MapUnit mu = map.get(name);
-                if(mu == null){
+                if (mu == null) {
                     continue;
                 }
                 mu.setSetMethodName(m.getName());
                 Type[] types = m.getGenericParameterTypes();
-                if(types == null || types.length != 1){
+                if (types == null || types.length != 1) {
                     throw new RuntimeException("标记的 SET 方法不合理，参数个数不是 1");
                 }
                 mu.setTarget(types[0]);
-                if(!mus.contains(mu)){
+                if (!mus.contains(mu)) {
                     mus.add(mu);
                 }
             }
@@ -126,9 +124,9 @@ public class ParseUtil {
         return mus;
     }
 
-    private static String getMapFieldName(Field field){
+    private static String getMapFieldName(Field field) {
         Expressions expressions = field.getAnnotation(Expressions.class);
-        if(expressions != null){
+        if (expressions != null) {
             return expressions.value();
         }
         return field.getName();
